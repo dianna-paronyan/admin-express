3b4bdda1eca97d22e3934e85adc034a7699c6036
dianna1234 <01234dianna@gmail.com>
2023-04-14 23:02:13 +0400

    add login validation

--- validation.js
+++ validation.js
@@ -17,4 +17,17 @@ const registerValidator = (data) => {
     return schema.validate(data);
 }
 
-module.exports = {registerValidator}
+const loginValidator = (data) => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .min(6)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(6)
+            .required()
+    });
+    return schema.validate(data);
+}
+
+module.exports = {registerValidator, loginValidator}
