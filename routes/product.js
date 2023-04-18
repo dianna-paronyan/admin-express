54aba5f4af9d1e8434937909bd60ee64021a02eb
dianna1234 <01234dianna@gmail.com>
2023-04-18 11:01:01 +0400

    add middleware in product routes

--- routes/product.js
+++ routes/product.js
@@ -4,10 +4,10 @@ function product_routes(app){
     const products_controller = require('../controllers/product_controller');
     
     
-    app.get('/products', authenticateToken, products_controller.allProducts);
-    app.post('/createProduct',authenticateToken, products_controller.createProduct);
-    app.put('/updateProduct/:id', authenticateToken, products_controller.updateProduct);
-    app.delete('/deleteProduct/:id', authenticateToken, products_controller.deleteProduct);
+    app.get('/products',  products_controller.allProducts);
+    app.post('/createProduct', authenticateToken, products_controller.createProduct);
+    app.put('/updateProduct/:id', authenticateToken,  products_controller.updateProduct);
+    app.delete('/deleteProduct/:id',authenticateToken, products_controller.deleteProduct);
 }
 
 module.exports = {
