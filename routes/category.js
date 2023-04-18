379c1475d3ed9b88f74e02838f61056be1e9fba2
dianna1234 <01234dianna@gmail.com>
2023-04-18 11:02:01 +0400

    delete all catetgories middlware

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,7 @@ const { authenticateToken } = require('../jwt/jwt_authenticate');
 function category_routes(app){
     const categories_controller = require('../controllers/category_controller');
     
-    app.get('/categories', authenticateToken, categories_controller.allCategories);
+    app.get('/categories', categories_controller.allCategories);
     app.post('/createCategory', authenticateToken, categories_controller.createCategory);
     app.put('/updateCategory/:id', authenticateToken, categories_controller.updateCategory);
     app.delete('/deleteCategory/:id', authenticateToken, categories_controller.deleteCategory);
